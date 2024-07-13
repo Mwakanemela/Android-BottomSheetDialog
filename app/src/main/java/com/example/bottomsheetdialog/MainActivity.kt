2f2d25d1cffef9332a720c0ab7010680efa7c7e7
Mwakanemela <kayangemwakanemela@gmail.com>
2024-07-13 01:57:21 -0700

    created a default alert dialog for delete option in the bottom sheet dialog

--- app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.bottomsheetdialog
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -28,9 +31,43 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             val view: View = layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
+            val deleteFeedLayout: LinearLayout = view.findViewById(R.id.deleteFeedLayout)
+
+            deleteFeedLayout.setOnClickListener {
+                showDeleteConfirmationDialog()
+            }
             val dialog = BottomSheetDialog(this)
             dialog.setContentView(view)
             dialog.show()
         }
+    }
+    @SuppressLint("InflateParams")
+    private fun showDeleteConfirmationDialog() {
+        val inflater = LayoutInflater.from(this)
+        val customTitleView: View = inflater.inflate(R.layout.delete_title_custom_layout, null)
+        val builder = AlertDialog.Builder(this)
+//        builder.setTitle("Delete Feed Confirmation")
+        builder.setCustomTitle(customTitleView)
+        builder.setMessage("Are you sure you want to delete this feed?")
+
+        // Positive Button
+        builder.setPositiveButton("Delete") { dialog, which ->
+            // Handle delete action
+            handleDeleteAction()
+        }
+
+        // Negative Button
+        builder.setNegativeButton("Cancel") { dialog, which ->
+            dialog.dismiss() // Dismiss the dialog
+        }
+
+        // Create and show the AlertDialog
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    private fun handleDeleteAction() {
+        // Logic to delete the item
+        // e.g., remove it from a list or database
     }
 }

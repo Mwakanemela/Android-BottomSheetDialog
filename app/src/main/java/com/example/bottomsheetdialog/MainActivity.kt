aa6a1127a7489ae4ee5d0f234c0724eecd8da66b
Mwakanemela <kayangemwakanemela@gmail.com>
2024-07-13 00:30:14 -0700

    A simple bottom sheet dialog

--- app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.bottomsheetdialog
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+
+    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val view: View = layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(view)
+            dialog.show()
         }
     }
 }
